de0d622bb9ca5dd9ca9f2efb57d2081a22d44c25
Stuart <stuart@Stuarts-MacBook-Air.local>
2020-04-16 05:14:22 -0500

    Add constraints for part 1

---
+++ Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  HobbyTracker
+//
+//  Created by Stuart on 4/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  POIsTableViewController.swift
+//  GrailDiary
+//
+//  Created by Stuart on 4/12/20.
+//  Copyright © 2020 Stuart. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
